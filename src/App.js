213e24986129696247668521c1d920f436d0baf9
arjunuvladimer <arjunuvladimer@gmail.com>
2016-08-14 05:30:00 +0530

    Shop Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/home/homepage.component';
-import { ShopPage } from './pages/shop/shoppage.component';
+import ShopPage  from './pages/shop/shoppage.component';
 import { Header } from './components/header/header.component.jsx'
 
 function App() {

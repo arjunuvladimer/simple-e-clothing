4ae675c94623938a9fbafdd6c6425bddec86ef3f
arjunuvladimer <arjunuvladimer@gmail.com>
2016-08-14 05:30:00 +0530

    Header Component with Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage.component';
+import {Switch, Route} from 'react-router-dom'
+import HomePage from './pages/home/homepage.component';
+import { ShopPage } from './pages/shop/shoppage.component';
+import { Header } from './components/header/header.component.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <>
+    <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/shop/shoppage.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './shoppage.styles.scss'
+
+export const ShopPage = () => (
+    <div>Shop Page</div>
+)

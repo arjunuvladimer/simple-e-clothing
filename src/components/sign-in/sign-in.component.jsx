fa6b4a6900eb2538ce4949fe40c55a51a594f5b2
arjunuvladimer <arjunuvladimer@gmail.com>
2016-08-15 05:30:00 +0530

    Resuable Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import './sign-in.styles.scss'
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../cutom-button/custom-button.component'
 
 class SignIn extends React.Component{
     constructor(){
@@ -31,12 +33,10 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form>
-                    <input onChange={this.handleChange} name='email' type='email' value={email} required/>
-                    <label>Email</label>
-                    <input onChange={this.handleChange} name='password' type='password' value={password} required/>
-                    <label>Password</label>
-                    <input type='submit' value='Submit Form' />
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput handleChange={this.handleChange} label='Email' name='email' type='email' value={email} required/>
+                    <FormInput handleChange={this.handleChange} label='Password' name='password' type='password' value={password} required/>
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
         )

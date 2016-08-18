e8d154eda57fd8650fd711d5b61b3e8ecfc45b65
arjunuvladimer <arjunuvladimer@gmail.com>
2016-08-18 05:30:00 +0530

    Adding Cart Items to Cart Drop Down

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './cart-dropdown.styles.scss'
 import CustomButton from '../cutom-button/custom-button.component'
+import {connect} from 'react-redux'
+import {selectCartItems} from '../../redux/cart/cart.selectors'
+import {createStructuredSelector} from 'reselect'
+import CartItem from '../cart-item/cart-item.component'
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            Cart Items
+            {cartItems.map(cartItem => 
+                <CartItem key={cartItem.id} item={cartItem} />
+            )}
         </div>
         <CustomButton>Go To Checkout</CustomButton>
     </div>
 ) 
 
-export default CartDropDown
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
+})
+
+export default connect(mapStateToProps,null)(CartDropDown)
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,3 @@
-import { cartActionTypes } from "./cart.types"
 
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)

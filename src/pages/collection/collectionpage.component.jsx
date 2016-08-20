84f353482fde1d67ce8c8461c1902b3bd72c285b
arjunuvladimer <arjunuvladimer@gmail.com>
2016-08-20 05:30:00 +0530

    Collection Page with Styled Components

--- src/pages/collection/collectionpage.component.jsx
+++ src/pages/collection/collectionpage.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import './collectionpage.styles.scss'
+import {CollectionPageContainer, TitleContainer, ItemsContainer} from './collectionpage.styles'
 import {connect} from 'react-redux'
 import {selectCollection} from '../../redux/shop/shop.selector'
 import CollectionItem from '../../components/collection-item/collection-item.component'
 const CollectionPage = ({match, collection}) => {
     const {title,items} = collection
     return (
-        <div className='collection-page'>
-        <div className='title'>{title}</div>
-        <div className='items'>
+        <CollectionPageContainer>
+        <TitleContainer>{title}</TitleContainer>
+        <ItemsContainer>
             {items.map(item => 
                 <CollectionItem key={item.id} item={item} />
             )}
-        </div>
-        </div>
+        </ItemsContainer>
+        </CollectionPageContainer>
     )
 }
 const mapStateToProps = (state, ownProps) => ({
